67a5b2b2dd172eed0f9f2afdccd51c09bdf48340
cheewei1994 <cheewei3388@gmail.com>
2026-06-15 01:56:40 +0800

    minimal speed test fix

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,36 +1,31 @@
-import { categories } from "@/lib/categories";
-import Link from "next/link";
-import ArticleSlider from "./ArticleSlider";
 import { supabase } from "@/lib/supabase";
-export const revalidate = 3600;
 
 export default async function ArticlePage({
   params,
 }: {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = await params
+  const { id } = await params;
 
   console.log("HELLO TEST");
-  
-console.time("article-query");
+
+  console.time("article-query");
 
   const { data: article } = await supabase
-  .from("articles")
-  .select(`
-  id,
-  title
-`)
-  .eq("id", Number(id))
-  .single();
+    .from("articles")
+    .select(`
+      id,
+      title
+    `)
+    .eq("id", Number(id))
+    .single();
 
   console.timeEnd("article-query");
 
   return (
-  <main>
-    <h1>{article?.title}</h1>
-    <div>TEST</div>
-  </main>
-);
-
- 
+    <main>
+      <h1>{article?.title}</h1>
+      <div>TEST</div>
+    </main>
+  );
+}
