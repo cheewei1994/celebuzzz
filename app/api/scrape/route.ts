f74e733fb7753d007c2d893ea9ae43d6b221bf71
cheewei1994 <cheewei3388@gmail.com>
2026-06-16 04:20:48 +0800

    test limte raw lines

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -276,21 +276,18 @@ console.log(
   title =
     titleMatch?.[1]?.trim() || "";
     image = "";
-
+  console.log(lines);
   const contentLines = lines.filter(line => {
 
-    if (line.startsWith("#")) return false;
-    if (line.startsWith("[")) return false;
-    if (line.includes("http")) return false;
-    if (line.includes("Published Time")) return false;
-    if (line.includes("URL Source")) return false;
-    if (line.length < 8) return false;
-    if (line.includes("Markdown Content")) return false;
-    if (line.includes("NEXT VIDEO")) return false;
-    if (line.includes("Advertisement")) return false;
-    if (line.includes("Powered by")) return false;
-    if (line.includes("Pause Play")) return false;
-    if (line.includes("Mute")) return false;
+   if (line.startsWith("[")) return false;
+if (line.includes("Published Time")) return false;
+if (line.includes("URL Source")) return false;
+if (line.includes("Markdown Content")) return false;
+if (line.includes("NEXT VIDEO")) return false;
+if (line.includes("Advertisement")) return false;
+if (line.includes("Powered by")) return false;
+if (line.includes("Pause Play")) return false;
+if (line.includes("Mute")) return false;
 
     return true;
   });
