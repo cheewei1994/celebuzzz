7bc5558743d44d8d47e5695af2f2af63425d4160
cheewei1994 <cheewei3388@gmail.com>
2026-06-29 15:44:13 +0800

    Adjust mobile ad label spacing

--- app/components/AdRenderer.tsx
+++ app/components/AdRenderer.tsx
@@ -81,7 +81,7 @@ export default function AdRenderer({
   return (
   <div
   ref={containerRef}
-  className={`my-6 ${
+  className={`mt-2 mb-6 md:my-6 ${
     position === "article-auto"
   ? "w-screen md:w-full relative left-1/2 md:left-0 -translate-x-1/2 md:translate-x-0"
   : ""
